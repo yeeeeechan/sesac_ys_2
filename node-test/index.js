fa5e43687746517d0babd711a42323a44f3af467
yeeeeechan <leeyechan6@gmail.com>
2023-10-10 18:44:28 +0900

    study : node module

--- node-test/index.js
+++ node-test/index.js
@@ -1,19 +1,24 @@
+// ----------------- case 3) ---------------
+// 파일에서 여러 개의 식별자를 객체 형태로 내보내는 경우
+// 특정 식별자(add, minus) 만 필요한 경우 객체구조분해 할당 문법을 통해 받아올 수 있다.
+const { add, minus } = require("./math.js");
 
-// ------------case 3-----------------
-const { add, minus } = require("./math");
 const sum = add(2, 3);
-console.log(sum)
-console.log(pi) // 객체구조분해 할당으로 add와 minus만 받아왔으므로, undefined 뜸
+console.log(sum);
+// console.log(PI); // 오류 발생
 
-// ------------case 2-----------------
+// ----------------- case 2) ---------------
+// 파일에서 여러 개의 식별자를 객체 형태로 내보내는 경우
+// 모듈을 받아올 때 객체를 그대로 math라는 식별자로 받음.
 // const math = require("./math.js");
 
-// const sum = math.add(3, 10);
+// const sum = math.add(3, 5);
 // console.log(sum);
-// console.log(math.pi);
+// console.log(math.PI);
 
-// ------------case 1-----------------
-// const add1 = require("./math.js"); // require은 모듈을 불러오는 함수, 이 코드는 그렇게 불러온 결과를 add1에 담는다는 뜻
+// ----------------- case 1) ---------------
+// 파일에서 한개의 식별자만 내보내는 경우
+// const add = require("./math.js");
 
-// const sum = add1(2, 3);
+// const sum = add(2, 3);
 // console.log(sum);
--- node-test/math.js
+++ node-test/math.js
@@ -1,25 +1,23 @@
 // 파일에서 여러 개의 식별자를 객체 형태로 내보내는 경우(중괄호 써서 사용하는 것과 동일한 결과)
-module.exports.add = (a, b) => a+b;
-module.exports.minus = (a, b) => a-b;
-module.exports.pi = 3.141592;
+// module.exports.add = (a, b) => a+b;
+// module.exports.minus = (a, b) => a-b;
+// module.exports.pi = 3.141592;
 
 
-// ------------case 3-----------------
-// 파일에서 여러 개의 식별자를 객체 형태로 내보내는 경우
-// 특정 식별자만 필요하다면, 객체구조분해 문법을 통해 불러온다.(index에서)
-
-// ------------case 2-----------------
+// ------------case 2, 3-----------------
 // 파일에서 여러 개의 식별자를 객체 형태로 내보내는 경우
 // 모듈을 받아올 때 객체를 그대로 math라는 식별자로 받음
-// const add = (a,b) => a+b;
-// const minus = (a, b) => a-b;
-// const pi = 3.141592;
+// 특정 식별자만 필요하다면, 객체구조분해 문법을 통해 불러온다.(index에서)
 
-// module.exports = {
-//     add,
-//     minus,
-//     pi
-// };
+const add = (a,b) => a+b;
+const minus = (a, b) => a-b;
+const pi = 3.141592;
+
+module.exports = {
+    add,
+    minus,
+    pi
+};
 // 객체는 보통 key:value 형태인데 key만 있는 것은 각각 'add라는 이름(key)으로 add함수를 넘긴다.'...는 뜻
 
 
